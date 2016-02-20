5c74638b0361d3605e6d1ae417e6218b4e29234a
Beisenbek Baisakov <beisenbek@gmail.com>
2016-02-20 13:53:52 +0600

    gp added

---
+++ Example2/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example2
+{
+    public partial class Form1 : Form
+    {
+        GraphicsPath gp = new GraphicsPath();
+
+        Rectangle r = new Rectangle(0, 0, 40, 40);
+        Timer t = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            t.Interval = 1000;
+            t.Tick += v;
+            t.Start();
+
+            gp.AddLine(new PointF(0, 0), new PointF(19, 19));
+            gp.AddLine(new PointF(10, 0), new PointF(19, 19));
+            gp.AddLine(new PointF(0, 10), new PointF(19, 19));
+            gp.AddLine(new PointF(20, 0), new PointF(19, 19));
+
+
+        }
+
+        int dx = 1;
+        int dy = 1;
+
+        int speed = 10;
+        Matrix m = new Matrix();
+
+        private void v(object sender, EventArgs e)
+        {
+            m.Translate(10, 10);
+
+
+            gp.Transform(m);
+
+
+            if (r.X + dx * speed + r.Width > Width)
+            {
+                dx = -1;
+            }
+            if (r.Y + dy * speed + r.Height > Height)
+            {
+                dy = -1;
+            }
+
+            if (r.X + dx * speed < 0)
+            {
+                dx = 1;
+            }
+            if (r.Y + dy * speed < 0)
+            {
+                dy = 1;
+            }
+
+            r.X += dx * speed;
+            r.Y += dy * speed;
+
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+
+            e.Graphics.DrawPath(new Pen(Color.Green), gp);
+
+            e.Graphics.FillEllipse(new SolidBrush(Color.Red), r);
+            e.Graphics.DrawRectangle(new Pen(Color.Green, 2), 0, 0, Width, Height);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
+
---
+++ Example3/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example3
+{
+    public partial class Form1 : Form
+    {
+        Rectangle r = new Rectangle(0, 0, 40, 40);
+
+        Graphics g;
+        Bitmap bmp;
+        int dx = 1;
+        int dy = 1;
+
+        int speed = 1;
+
+
+        Timer t = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            g = Graphics.FromImage(bmp);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+          
+            t.Tick += T_Tick;
+            t.Interval = 10;
+            t.Start();
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            if (r.X + dx * speed + r.Width > Width)
+            {
+                dx = -1;
+            }
+            if (r.Y + dy * speed + r.Height > Height)
+            {
+                dy = -1;
+            }
+
+            if (r.X + dx * speed < 0)
+            {
+                dx = 1;
+            }
+            if (r.Y + dy * speed < 0)
+            {
+                dy = 1;
+            }
+
+            r.X += dx * speed;
+            r.Y += dy * speed;
+
+
+            g.Clear(Color.White);
+            g.FillEllipse(new SolidBrush(Color.Red), r);
+
+            pictureBox1.Image = bmp;
+        }
+    }
+}
